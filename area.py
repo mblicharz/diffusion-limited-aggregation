698c6ec1af5895f4c472beef27c009b5213840d1
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-06-17 16:10:28 +0200

    Add Area and Point classes.

---
+++ area.py
@@ -0,0 +1,45 @@
+import random
+from typing import Tuple
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Point:
+    def __init__(self, x: int = 0, y: int = 0):
+        if x < 0 or y < 0:
+            raise ValueError("Both x and y should be greater or equal 0.")
+        self.x = x
+        self.y = y
+
+
+class Area:
+    def __init__(self, size: Tuple[int, int] = (0, 0)):
+        self.size = size
+        self.area = np.zeros((self.size[0], self.size[1]))
+
+    def set_point(self, point: Point):
+        self.area[point.x][point.y] = 1
+
+    def point_exists(self, point: Point) -> bool:
+        return bool(self.area[point.x][point.y])
+
+    def get_random_edge_point(self) -> Point:
+        edge = random.randint(1, 4)
+
+        if edge == 1:
+            return Point(0, random.randint(0, self.size[1]) - 1)
+
+        elif edge == 2:
+            return Point(random.randint(0, self.size[0] - 1), self.size[1] - 1)
+
+        elif edge == 3:
+            return Point(self.size[0] - 1, random.randint(0, self.size[1] - 1))
+
+        elif edge == 4:
+            return Point(random.randint(0, self.size[0] - 1), 0)
+
+    def plot(self) -> None:
+        plt.ylim(0, self.size[0] - 1)
+        plt.xlim(0, self.size[1] - 1)
+        plt.imshow(self.area)
+        plt.show()
