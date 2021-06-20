ca99a0f90af321fa2169dc8236103790207d4787
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-06-20 13:48:48 +0200

    Refactor DLA class.

--- diffusion_limited_aggregation.py
+++ diffusion_limited_aggregation.py
@@ -1,11 +1,19 @@
-from typing import Tuple, List
+from typing import Tuple
+
+from area import Area, Point
 
 
 class DiffusionLimitedAggregation:
-    def __init__(self, area_size: List[int, int] = None,
-                 seed: Tuple[int, int] = None):
+    def __init__(self, area_size: Tuple[int, int] = (0, 0),
+                 seed: Point = None, particles_num: int = 0):
         self.area_size = area_size
+        self.particles_num = particles_num
         if seed:
             self.seed = seed
         else:
-            self.seed = [self.area_size[0] / 2, self.area_size[1] / 2]
+            self.seed = Point(
+                round(self.area_size[0] / 2),
+                round(self.area_size[1] / 2))
+
+    def draw(self):
+        pass
