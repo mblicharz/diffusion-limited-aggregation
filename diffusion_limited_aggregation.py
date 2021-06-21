453864acf2334d38f1caa8cc058f4fc09921794a
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-06-21 15:05:59 +0200

    Add parameters for lattice in DLA class constructor.

--- diffusion_limited_aggregation.py
+++ diffusion_limited_aggregation.py
@@ -1,11 +1,13 @@
 from typing import Tuple
 
-from area import Area, Point
+from area import Area, Point, SquareLattice
 
 
 class DiffusionLimitedAggregation:
     def __init__(self, size: Tuple[int, int] = (0, 0),
-                 seed: Point = None, particles_num: int = 0):
+                 seed: Point = None, particles_num: int = 0,
+                 lattice_size: int = None, lattice_step: int = None,
+                 lattice_shape: str = 'square'):
         self.area = Area(size)
         self.particles_num = particles_num
         if seed:
@@ -14,6 +16,14 @@ class DiffusionLimitedAggregation:
             self.seed = Point(
                 round(size[0] / 2),
                 round(size[1] / 2))
+
+        self.lattice = None
+        self.lattice_step = None
+        if lattice_size:
+            self.lattice_step = lattice_step
+            if lattice_shape == 'square':
+                self.lattice = SquareLattice(self.seed, size)
+            self.lattice.increase_size(self.lattice_step)
 
     def show(self) -> None:
         self.area.plot()
