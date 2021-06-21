84ab643f5dc423084f17b2225ef83799a52eef70
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-06-21 18:14:25 +0200

    Add show_progress parameter for DLA class.

--- diffusion_limited_aggregation.py
+++ diffusion_limited_aggregation.py
@@ -7,7 +7,7 @@ class DiffusionLimitedAggregation:
     def __init__(self, size: Tuple[int, int] = (0, 0),
                  seed: Point = None, particles_num: int = 0,
                  lattice_size: int = None, lattice_step: int = 10,
-                 lattice_shape: str = 'square'):
+                 lattice_shape: str = 'square', show_progress: bool = False):
         self.area = Area(size)
         self.particles_num = particles_num
         if seed:
@@ -25,14 +25,19 @@ class DiffusionLimitedAggregation:
                 self.lattice = SquareLattice(self.seed, size)
             self.lattice.increase_size(self.lattice_step)
 
+        self.show_progress = show_progress
+
     def show(self) -> None:
         self.area.plot()
 
     def draw(self) -> None:
         self.area.set_point(self.seed)
 
-        for _ in range(self.particles_num):
-            print(_)
+        for i in range(self.particles_num):
+            if self.show_progress:
+                print(f'{self.particles_num} / {i} ||'
+                      f' {round(i / self.particles_num * 100)}%')
+
             particle = self._random_edge_point()
 
             while True:
