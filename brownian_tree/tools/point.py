852da39a84c0df61b977e91bec21e92c0a94fe42
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-06-25 17:56:53 +0200

    Add __eq__ method for Point class.

--- brownian_tree/tools/point.py
+++ brownian_tree/tools/point.py
@@ -7,3 +7,7 @@ class Point:
 
     def __repr__(self):
         return f'{self.x} {self.y}'
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
