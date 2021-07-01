6ce72de1680ae3635457cfe6069736ef36d90f97
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-01 15:34:29 +0200

    Remove main file.

--- main.py
+++
@@ -1,8 +0,0 @@
-from brownian_tree import get_brownian_tree
-
-if __name__ == "__main__":
-    brownian_tree = get_brownian_tree(size=(250, 250),
-                                      particles_num=12000,
-                                      )
-    brownian_tree.draw()
-    brownian_tree.show()
