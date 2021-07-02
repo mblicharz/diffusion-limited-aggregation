57d387998bc63d686c9c2367b64496eae6aadf92
Maciej Blicharz <maciej.blicharz@protonmail.com>
2021-07-02 17:11:45 +0200

    Remove types in docstring.

--- brownian_tree/__init__.py
+++ brownian_tree/__init__.py
@@ -12,26 +12,20 @@ def get_brownian_tree(size: Tuple[int, int],
     """Returns an object of Brownian Tree.
 
     :param size: The maximum size of the area on which the fractal will grow.
-    :type size: (int, int)
     :param seed: Point with x and y coordinates, from which the fractal growth
-    will start. By default, it is centered on the area.
-    :type seed: (int, int)
+        will start. By default, it is centered on the area.
     :param particles_num: The number of particles that the fractal will
-    consist of.
-    :type particles_num: int
-    :param lattice_size: The Initial size for lattice.
-    This parameter is mandatory if you want to use lattice.
-    :type lattice_size: int
+        consist of.
+    :param lattice_size: The Initial size for lattice. This parameter is
+        mandatory if you want to use lattice.
     :param lattice_step: The value by which the lattice will grow.
-    If 'lattice_size' is not set, this parameter will be ignored.
-    :type lattice_step: int
+        If 'lattice_size' is not set, this parameter will be ignored.
     :param lattice_shape: The shape of lattice. Available shapes are 'square'
-    and 'circle'. This affects the end result, e.g. for square lattice,
-    the fractal will be square. If 'lattice_size' is not set, this parameter
-    will be ignored.
-    :type lattice_shape: str
+        and 'circle'. This affects the end result, e.g. for square lattice,
+        the fractal will be square. If 'lattice_size' is not set,
+        this parameter will be ignored.
+
     :return: Object of BrownianTree.
-    :rtype: BrownianTree
     """
     bt = BrownianTree(
         size=size,
